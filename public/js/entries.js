59242d7547c01ffd6c3435944d1af70d1355bf2a
lewisevan <36089251+lewisevan@users.noreply.github.com>
2020-06-25 19:21:25 -0500

    Fixes edit entry bug
    https://lewismevan.freshrelease.com/BEMA/tasks/BEMA-100

--- public/js/entries.js
+++ public/js/entries.js
@@ -105,6 +105,8 @@ let editEntry = (e) => {
     for (key of e.target) {
         if (key.name === "edit_flagged" || key.name === "edit_disqualified") {
             body[key.name] = key.checked;
+        } else if (key.name === "edit_entry_group" && key.value === "null") {
+            body[key.name] = null;
         } else {
             body[key.name] = key.value;
         }
