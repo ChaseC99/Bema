0c0b9a873e045e08c7c768fe53b59b7ca67d2b02
lewisevan <36089251+lewisevan@users.noreply.github.com>
2020-08-26 09:58:13 -0500

    Updates login process

--- handlers/auth.js
+++ handlers/auth.js
@@ -1,116 +1,79 @@
-// Thanks Ethan for the OAuth example, very helpful:
-// https://github.com/EthanLuisMcDonough/ka-oauth-example
-
 const db = require(process.cwd() + "/util/db");
 const jwt = require("jsonwebtoken");
-const OAuthClient = require("oauth-1-client");
-const { createJWTToken, handleNext, jsonMessage } = require(process.cwd() + "/util/functions");
-const { KA_CONSUMER_KEY, KA_CONSUMER_SECRET, PORT } = process.env;
-const OAUTH_CALLBACK_PATH = `/api/auth/oauth_callback`;
-const KA = "www.khanacademy.org";
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const { createJWTToken, handleNext, successMsg, jsonMessage } = require(process.cwd() + "/util/functions");
 
-const client = (() => {
-    const callback = (
-        process.env.APP_STATE === "dev"
-        ? `http://localhost:${PORT}${OAUTH_CALLBACK_PATH}`
-        : `https://bema.herokuapp.com${OAUTH_CALLBACK_PATH}`
-    );
-    return new OAuthClient({
-        key: KA_CONSUMER_KEY,
-        secret: KA_CONSUMER_SECRET,
-        apiHostName: KA,
-        requestUrl: `https://${KA}/api/auth2/request_token?oauth_callback=${callback}`,
-        accessUrl: `https://${KA}/api/auth2/access_token`
-    });
-})();
-
-exports.connect = function(request, response, next) {
+exports.login = function(request, response, next) {
     if (request.decodedToken) {
-        // If token
         return handleNext(next, 400, "You are already logged in");
     }
-    // If no token, connect user like normal.
-    client.requestToken()
-        .then(r => `https://${KA}/api/auth2/authorize?oauth_token=${r.token}`)
-        .then(url => response.json({ redirect_url: url }))
-        .catch(err => handleNext(next, 400, "Problem while requesting token"));
+    let {
+        username,
+        password
+    } = request.body;
+    // If not logged in, attempt to connect user
+    db.query("SELECT username, password, evaluator_kaid, account_locked FROM evaluator WHERE username = $1", [username], res => {
+        if (res.error) {
+            return handleNext(next, 400, "There was a problem searching for this user");
+        }
+        if (res.rows.length === 0) {
+            // No user with the given username was found
+            return handleNext(next, 400, "Incorrect username or password");
+        } else {
+            bcrypt.compare(password, res.rows[0].password, function(err, result) {
+                if (result) {
+                    // Passwords match
+                    if (res.rows[0].account_locked) {
+                        return handleNext(next, 400, "This account has been disabled. Please contact an administrator for assistance.");
+                    } else {
+                        db.query("UPDATE evaluator SET logged_in_tstz = CURRENT_TIMESTAMP WHERE evaluator_kaid = $1", [res.rows[0].evaluator_kaid], result => {
+                            if (result.error) {
+                                return handleNext(next, 400, "There was a problem logging you in");
+                            }
+                            createJWTToken(res.rows[0].evaluator_kaid)
+                              .then(jwtToken => {
+                                  response.cookie("jwtToken", jwtToken, { expires: new Date(Date.now() + 86400000) });
+                                  successMsg(response);
+                              })
+                              .catch(err => handleNext(next, 400, err.message));
+                        });
+                    }
+                } else {
+                    // Passwords do not match
+                    return handleNext(next, 400, "Incorrect username or password");
+                }
+            });
+        }
+
+    });
 }
 
-exports.oauthCallback = function(request, response, next) {
-    // URL has the query strings
-    const { query } = request;
-    if (query && query.oauth_token && query.oauth_token_secret && query.oauth_verifier) {
-        // Get an access token.
-        client.accessToken(
-            query.oauth_token,
-            query.oauth_token_secret,
-            query.oauth_verifier
-        ).then(r => {
-            // r has "token", "tokenSecret", and "query".
-            const { token, tokenSecret } = r;
-            // Use access tokens to get user info.
-            client.auth(token, tokenSecret)
-                .get("/api/internal/user", { casing: "camel" })
-                .then(httpResponse => httpResponse.body)
-                .then(userInfo => {
-                    // userInfo is object with user info.
-                    let { nickname, email, username, kaid, avatarUrl } = userInfo;
-                    db.query("SELECT * FROM whitelisted_kaids WHERE kaid = $1", [kaid], result => {
-                        if (result.error) {
-                            return handleNext(next, 400, "There was a problem while checking if this KAID is whitelisted");
-                        }
-                        // If row, truthy, kaid is whitelisted, move on.
-                        if (result.rows.length) {
-                            // let kaidNums = kaid.split("_")[1];
-                            db.query("SELECT * FROM evaluator WHERE evaluator_kaid = $1", [kaid], result => {
-                                if (result.error) {
-                                    return handleNext(next, 400, "There was a problem while searching for this evaluator_kaid, please try again");
-                                }
-                                // If row, truthy, user exists, just log in.
-                                if (result.rows.length) {
-                                  if (result.rows[0].account_locked) {
-                                    // Account has been disabled, so do not allow the user to log in
-                                    response.redirect("/login");
-                                  } else {
-                                      db.query("UPDATE evaluator SET logged_in_tstz = CURRENT_TIMESTAMP WHERE evaluator_kaid = $1", [kaid], result => {
-                                          if (result.error) {
-                                              return handleNext(next, 400, "There was a problem logging you in");
-                                          }
-                                          createJWTToken(kaid, token, tokenSecret)
-                                            .then(jwtToken => {
-                                                response.cookie("jwtToken", jwtToken, { expires: new Date(Date.now() + 86400000) });
-                                                response.redirect("/");
-                                            })
-                                            .catch(err => handleNext(next, 400, err.message));
-                                      });
-                                  }
-                                } else {
-                                    // User doesn't exist, sign up.
-                                    // Inserting this data would normally be dangerous, but Node.PG auto-sanitizes.
-                                    // https://github.com/brianc/node-postgres/wiki/FAQ#8-does-node-postgres-handle-sql-injection
-                                    db.query("INSERT INTO evaluator (logged_in_tstz, dt_term_start, dt_term_end, email, username, nickname, evaluator_name, evaluator_kaid, avatar_url) VALUES (CURRENT_TIMESTAMP, null, null, $1, $2, $3, $4, $5, $6)", [email, username, nickname, nickname, kaid, avatarUrl], result => {
-                                        if (result.error) {
-                                            return handleNext(next, 400, "There was a problem creating your account, please try again");
-                                        }
-                                        // This user was just created, now get their ID.
-                                        createJWTToken(kaid, token, tokenSecret)
-                                            .then(jwtToken => {
-                                                response.cookie("jwtToken", jwtToken, { expires: new Date(Date.now() + 86400000) });
-                                                response.redirect("/");
-                                            })
-                                            .catch(err => handleNext(next, 400, err.message));
-                                    });
-                                }
-                            });
-                        } else {
-                            // Not whitelisted
-                            response.redirect("/login");
-                        }
-                    });
-                }).catch(err => handleNext(next, 400, "Problem with getting KA user info"));
-        }).catch(err => handleNext(next, 400, "Problem while getting KA access token"));
+exports.changePassword = function(request, response, next) {
+    if (request.decodedToken) {
+        let {
+            evaluator_id,
+            new_password
+        } = request.body;
+        evaluator_id = parseInt(evaluator_id);
+        // Check that user is modifying own password or is an admin user
+        if (evaluator_id === request.decodedToken.evaluator_id || request.decodedToken.is_admin) {
+            bcrypt.hash(new_password, saltRounds, function(err, hash) {
+                if (err) {
+                    handleNext(next, 400, "Error changing password");
+                }
+                db.query("UPDATE evaluator SET password = $1 WHERE evaluator_id = $2", [hash, evaluator_id], result => {
+                    if (result.error) {
+                        return handleNext(next, 400, "There was a problem changing your password");
+                    }
+                    successMsg(response);
+                });
+            });
+        } else {
+            handleNext(next, 403, "Insufficient access");
+        }
     } else {
-        handleNext(next, 400, "Bad request");
+        handleNext(next, 403, "Insufficient access");
     }
 }
 
