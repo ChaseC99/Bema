4a6d8f05b435392ef26c1444800852b5d0dcb115
lewisevan <36089251+lewisevan@users.noreply.github.com>
2020-12-20 15:07:20 -0600

    Updates script linking

--- public/js/header.js
+++ public/js/header.js
@@ -1,16 +1,3 @@
-let request = (method = "post", path, data, callback) => {
-    fetch(path, (method === "get") ? null : {
-        method,
-        body: JSON.stringify(data),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
-    .then(res => res.json())
-    .then(data => callback(data))
-    .catch(err => alert(err)); // Will change later.
-};
-
 let logout = e => {
     e.preventDefault();
     fetch("/api/auth/logout", {
