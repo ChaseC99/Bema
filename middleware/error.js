d818e7fcfed5b2953c3c11dbd86b79f89d62b647
lewisevan <36089251+lewisevan@users.noreply.github.com>
2021-01-21 16:20:05 -0600

    Update error.js
    Only logs errors for logged in users to prevent spam.

--- middleware/error.js
+++ middleware/error.js
@@ -1,8 +1,8 @@
 const db = require("../util/db");
 
 const errorHandler = (data, request, response, next) => {
-	if (data.status && data.status !== 401 && data.status !== 403 && data.status !== 404) {
-		return db.query("SELECT log_error($1, $2, $3, $4, $5, $6);", [data.message, data.error.stack, request.decodedToken ? request.decodedToken.evaluator_id : null, request.headers.origin, request.headers.referer, request.headers['user-agent']], res => {
+	if (data.status && data.status !== 401 && data.status !== 403 && data.status !== 404 && request.decodedToken) {
+		return db.query("SELECT log_error($1, $2, $3, $4, $5, $6);", [data.message, data.error.stack, request.decodedToken.evaluator_id, request.headers.origin, request.headers.referer, request.headers['user-agent']], res => {
 			return response.status(data.status || 500).json({
 				error: {
 					status: data.status,
