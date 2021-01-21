7b58ad6afc9012b4e24e210f1ac80c29c89d72f4
lewisevan <36089251+lewisevan@users.noreply.github.com>
2021-01-20 21:32:55 -0600

    BEMA-186 Logs additional error info

--- middleware/error.js
+++ middleware/error.js
@@ -2,7 +2,7 @@ const db = require("../util/db");
 
 const errorHandler = (data, request, response, next) => {
 	if (data.status !== 401 && data.status !== 403) {
-		return db.query("SELECT log_error($1, $2);", [data.message, data.error.stack], res => {
+		return db.query("SELECT log_error($1, $2, $3, $4, $5, $6);", [data.message, data.error.stack, request.decodedToken ? request.decodedToken.evaluator_id : null, request.headers.origin, request.headers.referer, request.headers['user-agent']], res => {
 			return response.status(data.status || 500).json({
 				error: {
 					status: data.status,
