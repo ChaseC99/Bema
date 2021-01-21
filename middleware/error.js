3f53ccc5a8a9c92435131f772f6c57a817b5e59f
lewisevan <36089251+lewisevan@users.noreply.github.com>
2021-01-20 20:49:16 -0600

    BEMA-186 Updates error handler

--- middleware/error.js
+++ middleware/error.js
@@ -1,9 +1,25 @@
-const errorHandler = (error, request, response, next) => {
-	return response.status(error.status || 500).json({
-		error: {
-			message: error.message || "Oops! Something went REALLY wrong.",
-		},
-	});
+const db = require("../util/db");
+
+const errorHandler = (data, request, response, next) => {
+	if (data.status !== 401 && data.status !== 403) {
+		return db.query("SELECT log_error($1, $2);", [data.message, data.error.stack], res => {
+			return response.status(data.status || 500).json({
+				error: {
+					status: data.status,
+					message: data.message || "Unknown error",
+					id: res.rows[0].log_error
+				},
+			});
+		});
+	} else {
+		return response.status(data.status || 500).json({
+			error: {
+				status: data.status,
+				message: data.message || "Unknown error",
+				id: null
+			},
+		});
+	}
 }
 
 module.exports = errorHandler;
