2664c5eb2d1bc922b4208cc30b0eceb560b6e805
lewisevan <36089251+lewisevan@users.noreply.github.com>
2021-02-07 10:42:37 -0600

    BEMA-199 Adds edit permissions API route

--- handlers/api/users.js
+++ handlers/api/users.js
@@ -283,4 +283,61 @@ exports.getPermissions = (request, response, next) => {
     }
 };
 
+exports.editPermissions = (request, response, next) => {
+    try {
+        if (request.decodedToken) {
+            let {
+                evaluator_id,
+                view_admin_stats,
+                edit_contests,
+                delete_contests,
+                edit_entries,
+                delete_entries,
+                add_entries,
+                assign_entry_groups,
+                view_all_evaluations,
+                edit_all_evaluations,
+                delete_all_evaluations,
+                manage_winners,
+                view_all_tasks,
+                edit_all_tasks,
+                delete_all_tasks,
+                view_judging_settings,
+                manage_judging_groups,
+                assign_evaluator_groups,
+                manage_judging_criteria,
+                view_all_users,
+                edit_user_profiles,
+                change_user_passwords,
+                assume_user_identities,
+                add_users,
+                view_errors,
+                delete_errors,
+                judge_entries,
+                edit_kb_content,
+                delete_kb_content,
+                publish_kb_content,
+                manage_announcements
+            } = request.body;
+            let {
+                is_admin
+            } = request.decodedToken;
+
+            if (is_admin) {
+                return db.query("UPDATE evaluator_permissions SET view_admin_stats = $2, edit_contests = $3, delete_contests = $4, edit_entries = $5, delete_entries = $6, add_entries = $7, assign_entry_groups = $8, view_all_evaluations = $9, edit_all_evaluations = $10, delete_all_evaluations = $11, manage_winners = $12, view_all_tasks = $13, edit_all_tasks = $14, delete_all_tasks = $15, view_judging_settings = $16, manage_judging_groups = $17, assign_evaluator_groups = $18, manage_judging_criteria = $19, view_all_users = $20, edit_user_profiles = $21, change_user_passwords = $22, assume_user_identities = $23, add_users = $24, view_errors = $25, delete_errors = $26, judge_entries = $27, edit_kb_content = $28, delete_kb_content = $29, publish_kb_content = $30, manage_announcements = $31 WHERE evaluator_id = $1", [evaluator_id, view_admin_stats, edit_contests, delete_contests, edit_entries, delete_entries, add_entries, assign_entry_groups, view_all_evaluations, edit_all_evaluations, delete_all_evaluations, manage_winners, view_all_tasks, edit_all_tasks, delete_all_tasks, view_judging_settings, manage_judging_groups, assign_evaluator_groups, manage_judging_criteria, view_all_users, edit_user_profiles, change_user_passwords, assume_user_identities, add_users, view_errors, delete_errors, judge_entries, edit_kb_content, delete_kb_content, publish_kb_content, manage_announcements], res => {
+                    if (res.error) {
+                        return handleNext(next, 400, "There was a problem updating this user's permissions.", res.error);
+                    }
+                    successMsg(response);
+                });
+            } else {
+                return handleNext(next, 403, "You're not authorized to edit user permissions.");
+            }
+        }
+        return handleNext(next, 401, "You must log in to edit user permissions.");
+    } catch (error) {
+        return handleNext(next, 500, "Unexpected error while updating user permissions.", error);
+    }
+}
+
 module.exports = exports;
