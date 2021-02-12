c838b6ec00bb177ebe0bfa50edbbf81f6f84307e
lewisevan <36089251+lewisevan@users.noreply.github.com>
2021-02-11 22:45:39 -0600

    BEMA-224 Updates results page styling

--- public/js/results.js
+++ public/js/results.js
@@ -29,12 +29,13 @@ request("get", "/api/internal/contests", null, (data) => {
     if (!data.error) {
         sidebarSpinner.style.display = "none";
         resultsContestName.textContent = `Results for ${data.contests.filter(c => c.contest_id == currentContestId)[0].contest_name}`;
+        sidebar.innerHTML += `<h3>Contest</h3>`;
         data.contests.forEach(c => {
             sidebar.innerHTML += `
                 <a class="nav-button" href="/results/${c.contest_id}" id="contest-tab-${c.contest_id}">
-                    <i class="fas fa-trophy"></i>
+
                     <p>
-                        ${c.contest_name}
+                        ${c.contest_name.split("Contest: ")[1]}
                     </p>
                 </a>
             `;
@@ -49,7 +50,7 @@ request("get", "/api/internal/contests", null, (data) => {
 request("get", `/api/internal/results?contestId=${currentContestId}`, null, (data) => {
     if (!data.error) {
         entriesPerLevelSpinner.style.display = "none";
-        entriesPerLevelTable.style.display = "block";
+        entriesPerLevelTable.style.display = "table";
         data.results.entriesPerLevel.forEach(a => {
             entriesPerLevelTableBody.innerHTML += `
             <tr>
@@ -62,7 +63,7 @@ request("get", `/api/internal/results?contestId=${currentContestId}`, null, (dat
             </tr>`;
         });
         evalsPerEvaluatorSpinner.style.display = "none";
-        evalsPerEvaluatorTable.style.display = "block";
+        evalsPerEvaluatorTable.style.display = "table";
         data.results.evaluationsPerEvaluator.forEach(a => {
             let entriesInGroup;
             if (data.logged_in) {
@@ -85,7 +86,7 @@ request("get", `/api/internal/results?contestId=${currentContestId}`, null, (dat
             </tr>`;
         });
         winnersSpinner.style.display = "none";
-        winnersTable.style.display = "block";
+        winnersTable.style.display = "table";
         data.results.winners.forEach(a => {
             winnersTableBody.innerHTML += `
             <tr>
@@ -104,15 +105,18 @@ request("get", `/api/internal/results?contestId=${currentContestId}`, null, (dat
                     ${a.entry_level}
                 </td>
                 ${data.is_admin || permissions.manage_winners
-                    ? `<td id="${a.entry_id}-actions">
-                           <i class="control-btn red far fa-trash-alt" onclick="deleteWinner(${a.entry_id})"></i>
+                    ? `<td class="actions">
+                            <i class="actions-dropdown-btn" onclick="showActionDropdown('winner-dropdown-${a.entry_id}');"></i>
+                            <div class="actions-dropdown-content" hidden id="winner-dropdown-${a.entry_id}">
+                                <a href="#" onclick="showConfirmModal('Remove Winner?', 'Are you sure you want to remove this winner? Remember that public users can see this change.', 'deleteWinner(${a.entry_id})', false, 'Remove')">Remove</a>
+                            </div>
                        </td>`
                     : ""
                 }
             </tr>`;
         });
         entriesByAvgScoreSpinner.style.display = "none";
-        entriesByAvgScoreTable.style.display = "block";
+        entriesByAvgScoreTable.style.display = "table";
         data.results.entriesByAvgScore.forEach(a => {
             entriesByAvgScoreTableBody.innerHTML += `
             <tr>
@@ -139,8 +143,11 @@ request("get", `/api/internal/results?contestId=${currentContestId}`, null, (dat
                     </td>` : ""
                 }
                 ${data.is_admin || permissions.manage_winners ? `
-                    <td>
-                        <i class="fas fa-trophy" onclick="addWinner(${a.entry_id})"></i>
+                    <td class="actions">
+                        <i class="actions-dropdown-btn" onclick="showActionDropdown('entry-dropdown-${a.entry_id}');"></i>
+                        <div class="actions-dropdown-content" hidden id="entry-dropdown-${a.entry_id}">
+                            <a href="#" onclick="addWinner(${a.entry_id})">Mark as winner</a>
+                        </div>
                     </td>
                     ` : ""
                 }
@@ -164,17 +171,13 @@ let addWinner = (entry_id) => {
 };
 
 let deleteWinner = entry_id => {
-    let shouldDelete = confirm("Are you sure you want to delete this winner?");
-
-    if (shouldDelete) {
-        request("delete", "/api/internal/winners", {
-            entry_id
-        }, (data) => {
-            if (!data.error) {
-                window.setTimeout(() => window.location.reload(), 1000);
-            } else {
-                displayError(data.error);
-            }
-        });
-    }
+    request("delete", "/api/internal/winners", {
+        entry_id
+    }, (data) => {
+        if (!data.error) {
+            window.setTimeout(() => window.location.reload(), 1000);
+        } else {
+            displayError(data.error);
+        }
+    });
 };
