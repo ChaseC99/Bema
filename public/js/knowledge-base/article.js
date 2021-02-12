2a5c48e9244107698abb5b9c29248d4e83a71b84
lewisevan <36089251+lewisevan@users.noreply.github.com>
2021-02-12 12:27:32 -0600

    BEMA-232 Updates KB article design

--- public/js/knowledge-base/article.js
+++ public/js/knowledge-base/article.js
@@ -2,7 +2,8 @@ let article_id = window.location.href.substring(window.location.href.lastIndexOf
 let articleContainer = document.getElementById("article-container");
 let viewArticleContainer = document.getElementById("view-article-container")
 let editArticleContainer = document.getElementById("edit-article-container")
-let editArticleSectionDropdown = document.getElementById("edit-article-section-dropdown");
+let editArticleSectionDropdown = document.querySelector("#article-section-dropdown .select-dropdown-content");
+let sections;
 
 /***** Loads page data *****/
 request("get", "/api/internal/kb/articles?articleId=" + article_id, null, (data) => {
@@ -16,20 +17,21 @@ request("get", "/api/internal/kb/articles?articleId=" + article_id, null, (data)
                     <div class="db-header">
                         <p>${a.article_name}</p>
                             ${data.is_admin || permissions.edit_kb_content || permissions.delete_kb_content ? `
-                                <span class="admin-controls">
-                                    ${permissions.edit_kb_content || permissions.publish_kb_content || data.is_admin ? `<i class="control-btn far fa-edit" onclick="showEditArticleForm(${a.article_id})"></i>` : ""}
-                                    ${permissions.delete_kb_content || data.is_admin ? `<i class="control-btn red far fa-trash-alt" onclick="deleteArticle(${a.article_id})"></i>` : ""}
-                                </span>
+                                <i class="actions-dropdown-btn" onclick="showActionDropdown('article-dropdown-${a.article_id}');"></i>
+                                <div class="actions-dropdown-content" hidden id="article-dropdown-${a.article_id}">
+                                    ${permissions.edit_kb_content || permissions.publish_kb_content || data.is_admin ? `<a href="#" onclick="showEditArticleForm(${a.article_id})">Edit</a>` : ""}
+                                    ${permissions.delete_kb_content || data.is_admin ? `<a href="#" onclick="showConfirmModal('Delete Article?', 'Are you sure you want to delete this article? This action cannot be undone.', 'deleteArticle(${a.article_id})', true, 'Delete')">Delete</a>` : ""}
+                                </div>
                             ` : ``}
                     </div>
                     <div class="preview-content">
                         ${a.article_content}
 
                          <div class="article-meta">
-                            <p>Last updated: ${a.last_updated}</p>
-                            <p>Created by: ${author_name}</p>
-                            ${data.is_admin || permissions.edit_kb_content || permissions.publish_kb_content ? `<p>Published: ${a.is_published ? "<span style='color: green'>True</span>" : "<span style='color: red'>False</span>"}</p>
-                            <p>Visibility: ${a.article_visibility}</p>
+                            <p><span class="label">Last Updated:</span> ${a.last_updated}</p>
+                            <p><span class="label">Created By:</span> ${author_name}</p>
+                            ${data.is_admin || permissions.edit_kb_content || permissions.publish_kb_content ? `<p><span class="label">Published:</span> ${a.is_published ? "Yes" : "No"}</p>
+                            <p><span class="label">Visibility:</span> ${a.article_visibility}</p>
                         </div>` : ``}
                     </div>
                 </div>
@@ -43,10 +45,11 @@ request("get", "/api/internal/kb/articles?articleId=" + article_id, null, (data)
 request("get", "/api/internal/kb/sections", null, (data) => {
     if (!data.error) {
         if (permissions.edit_kb_content || data.is_admin) {
+            sections = data.sections;
             data.sections.forEach((s, idx) => {
                 // Fill forms with sections
                 editArticleSectionDropdown.innerHTML += `
-                    <option value="${s.section_id}">${s.section_name}</option>
+                    <a href="#" onclick="setSelectValue('article-section', ${s.section_id}, '${s.section_name}');">${s.section_name}</a>
                 `;
             });
         }
@@ -78,19 +81,20 @@ let editArticle = (e) => {
 }
 
 let deleteArticle = (id) => {
-    let confirm = window.confirm("Are you sure you want to delete this article?");
-    if (confirm) {
-        request("delete", "/api/internal/kb/articles", {article_id: id}, (data) => {
-            if (!data.error) {
-                window.setTimeout(() => window.location.reload(), 1000);
-            } else {
-                displayError(data.error);
-            }
-        });
-    }
+    request("delete", "/api/internal/kb/articles", {article_id: id}, (data) => {
+        if (!data.error) {
+            window.setTimeout(() => window.location.reload(), 1000);
+        } else {
+            displayError(data.error);
+        }
+    });
 }
 
 /***** Show forms *****/
+let showArticle = () => {
+    editArticleContainer.style.display = "none";
+    viewArticleContainer.style.display = "block";
+}
 let showEditArticleForm = (id) => {
     viewArticleContainer.style.display = "none";
     editArticleContainer.style.display = "block";
@@ -98,11 +102,23 @@ let showEditArticleForm = (id) => {
     let editArticleForm = document.querySelector("#edit-article-form");
     request("get", "/api/internal/kb/articles?articleId=" + id, null, (data) => {
         data = data.article;
+        console.log(sections);
         editArticleForm["article_id"].value = data.article_id;
         editArticleForm["article_name"].value = data.article_name;
         editArticleForm["article_visibility"].value = data.article_visibility;
         editArticleForm["article_section"].value = data.section_id;
         editArticleForm["is_published"].checked = data.is_published;
+
+        setSelectValue('article-visibility', data.article_visibility, data.article_visibility);
+
+        let sectionName = "";
+        for (let i = 0; i < sections.length; i++) {
+            if (sections[i].section_id === data.section_id) {
+                sectionName = sections[i].section_name;
+                break;
+            }
+        }
+        setSelectValue('article-section', data.section_id, sectionName);
 
         document.querySelector("#edit-article-editor").firstChild.innerHTML = data.article_content;
     });
