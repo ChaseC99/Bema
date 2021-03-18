1890f780c072a0a4f519349637378f168a3895cf
lewisevan <36089251+lewisevan@users.noreply.github.com>
2021-03-17 21:35:26 -0700

    Update redirects.js

--- middleware/redirects.js
+++ middleware/redirects.js
@@ -2,6 +2,9 @@ const redirects = (request, response, next) => {
     if (request.headers.host === "bema.herokuapp.com") {
         return response.redirect("https://www.kachallengecouncil.org" + request.url);
     }
+    else if (request.headers['x-forwarded-proto'] !== 'https') {
+        return response.redirect('https://' + request.headers.host + request.url);
+    }
     else if (request.url === "/status") {
         return response.redirect("http://status.kachallengecouncil.org");
     }
