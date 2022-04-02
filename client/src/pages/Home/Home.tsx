8d4e2c3cf20293ede4b697af9a9cfffdb033dc98
Evan Lewis <36089251+lewisevan@users.noreply.github.com>
2022-04-02 14:42:14 -0500

    Add Home page

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,7 +1,17 @@
+import React from "react";
+import useAppState from "../../state/useAppState";
 import Announcements from "./Announcements";
+import Tasks from "./Tasks";
 
 function Home() {
-    return <Announcements />
+  const { state } = useAppState();
+
+  return (
+    <React.Fragment>
+      {state.logged_in && <Tasks />}
+      <Announcements />
+    </React.Fragment>
+  );
 }
 
 export default Home;
