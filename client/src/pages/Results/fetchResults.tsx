d3fa96139efcea4180abf0070c68db2fe64db58a
Evan Lewis <36089251+lewisevan@users.noreply.github.com>
2022-04-21 13:44:40 -0500

    Add results data fetching

---
+++ client/src/pages/Results/fetchResults.tsx
@@ -0,0 +1,9 @@
+import request from "../../util/request";
+
+export async function fetchResults(contestId: string) {
+    const data = await request("GET", "/api/internal/results?contestId=" + contestId);
+    return {
+        results: data.results,
+        voting_enabled: data.voting_enabled
+    }
+}
