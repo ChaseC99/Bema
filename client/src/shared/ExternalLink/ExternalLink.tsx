4d026f1c34e1ab900caaf31623ed4b800c788950
Evan Lewis <36089251+lewisevan@users.noreply.github.com>
2022-04-21 15:15:12 -0500

    Add ExternalLink component

---
+++ client/src/shared/ExternalLink/ExternalLink.tsx
@@ -0,0 +1,19 @@
+import React, { ReactChild } from "react";
+
+type ExternalLinkProps = {
+  to: string
+  target?: "_blank" | "_self" | "_parent" | "_top"
+  children: ReactChild | ReactChild[]
+}
+
+function ExternalLink(props: ExternalLinkProps) {
+  const handleClick = () => {
+    window.open(props.to, props.target || "_blank");
+  }
+
+  return (
+    <a href={props.to} target={props.target} onClick={handleClick} className="link-underline">{props.children}</a>
+  );
+}
+
+export default ExternalLink;
