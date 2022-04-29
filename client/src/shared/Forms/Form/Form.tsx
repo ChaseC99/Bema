3de524bd768780a546b4f00ba9fefbc900f16796
Evan Lewis <36089251+lewisevan@users.noreply.github.com>
2022-04-29 15:47:51 -0500

    Add generalized form styling
    Form styling is now distinguished between general forms, and forms within a modal.

--- client/src/shared/Forms/Form/Form.tsx
+++ client/src/shared/Forms/Form/Form.tsx
@@ -11,7 +11,7 @@ import "./Form.css";
 
 type FormProps = {
   onSubmit: (values: { [name: string]: any }) => void
-  onCancel: () => void
+  onCancel?: () => void
   fields: FormFields[]
   submitLabel: string
   testId?: string
@@ -211,7 +211,7 @@ function Form(props: FormProps) {
       </div>
 
       <div className="form-actions">
-        <Button type="tertiary" role="button" action={props.onCancel} text="Cancel" testId={props.testId + "-cancel"} />
+        {props.onCancel && <Button type="tertiary" role="button" action={props.onCancel} text="Cancel" testId={props.testId + "-cancel"} />}
         <Button type="primary" role="button" action={handleSubmit} text={props.submitLabel} testId={props.testId + "-submit"} disabled={Object.keys(errors).length > 0} />
       </div>
     </div>
