bcf91322c8fc5fbd560eb20706bd4a28cbdbd6fc
Evan Lewis <36089251+lewisevan@users.noreply.github.com>
2022-05-04 00:22:16 -0500

    Add shorter imports

---
+++ client/src/shared/Routes/index.tsx
@@ -0,0 +1,5 @@
+import AuthenticatedRoute from "./AuthenticatedRoute/AuthenticatedRoute";
+import ProtectedRoute from "./ProtectedRoute/ProtectedRoute";
+import UnauthenticatedRoute from "./UnauthenticatedRoute/UnauthenticatedRoute";
+
+export { AuthenticatedRoute, ProtectedRoute, UnauthenticatedRoute };
