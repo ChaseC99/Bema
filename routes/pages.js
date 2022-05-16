287f07f134b95995f8768a9c55abd8df66457f8c
Evan Lewis <36089251+lewisevan@users.noreply.github.com>
2022-05-16 18:35:47 -0500

    Remove old pages from backend

--- routes/pages.js
+++ routes/pages.js
@@ -4,5 +4,4 @@ const handlers = require(process.cwd() + "/handlers/pages");
 
 router.get("/admin/kb", handlers.kbHome);
 router.get("/admin/kb/article/:articleId", handlers.kbArticle);
-router.get("/evaluator/:userId", handlers.evaluatorProfile);
 module.exports = router;
