e1d44f847f29b8bfdb92d728f1b0b0026c6b3e3f
Evan Lewis <36089251+lewisevan@users.noreply.github.com>
2022-06-27 14:40:07 -0500

    Remove old backend code

--- middleware/log.js
+++
@@ -1,7 +0,0 @@
-const log = (request, response, next) => {
-    let time = new Date().toLocaleTimeString();
-    console.log(`${time}  -  ${request.method} ${request.url}`);
-    next();
-}
-
-module.exports = log;
