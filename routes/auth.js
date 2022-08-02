508771f1572fe85d677f4f9a9bbfe500234842e3
Evan Lewis <36089251+lewisevan@users.noreply.github.com>
2022-08-02 13:19:21 -0500

    Remove remaining old backend code

--- routes/auth.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const handlers = require(process.cwd() + "/handlers/auth");
-
-router.post("/assumeUserIdentity", handlers.assumeUserIdentity);
-
-module.exports = router;
