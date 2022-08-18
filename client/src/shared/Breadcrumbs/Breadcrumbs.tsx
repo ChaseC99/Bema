56b560ccd2b82cf6afb006ead4c2d91a8aee34d6
Evan Lewis <36089251+lewisevan@users.noreply.github.com>
2022-08-18 15:18:38 -0500

    Fix key prop error

--- client/src/shared/Breadcrumbs/Breadcrumbs.tsx
+++ client/src/shared/Breadcrumbs/Breadcrumbs.tsx
@@ -11,10 +11,10 @@ export default function Breadcrumbs(props: BreadcrumbProps) {
       {React.Children.toArray(props.children)[0]}
       {React.Children.toArray(props.children).slice(1).map((child, index) => {
         return (
-          <>
+          <React.Fragment key={index}>
             <svg width="16" height="16" viewBox="0 0 16 16"><circle cx="8" cy="9" r="1.5"></circle></svg>
             {child}
-          </>
+          </React.Fragment>
         );
       })}
     </div>
